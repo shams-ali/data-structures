400d465cbd69d5c925f81a357e7596529ebcb3f2
Shams Ali <ali.s90@outlook.com>
2016-04-20 14:38:37 -0700

    started queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
+  var len = 1;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+
+    storage[len] = value;
+    len--;
+    length++;
   };
 
   someInstance.dequeue = function() {
+    len++;
+    length--;
+    var res = storage[length + len];
+    return res;
   };
 
   someInstance.size = function() {
+    return length > 0 ? length : 0;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,20 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var length = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[length] = value;
+    length++;
   };
 
   someInstance.pop = function() {
+    length--;
+    return storage[length];
   };
 
   someInstance.size = function() {
+    return length > 0 ? length : 0;
   };
 
   return someInstance;
