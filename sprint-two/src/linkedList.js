64c73cbac2169f5c0fd306dbf711d86479ccadeb
shams-ali <shams.ali0216@gmail.com>
2016-04-21 16:40:28 -0700

    finished linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,48 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+
+    // create newNode(value)
+
+    var newNode = new Node (value);
+
+    // if (tail === null) head and tail === newNode
+    if (list.tail === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+    //else tail.next = newNode
+
+    //tail === newNode
+
+        
   };
 
   list.removeHead = function() {
+
+    var currentHead = list.head.value;
+
+    list.head = list.head.next;
+
+    return currentHead;
+
+
+
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, node) {
+    node = node || list.head;
+    // check if node.value is equal to target
+      // if so, return true
+    if (node.value === target) {
+      return true;
+    }
+    // check if the next property is an object
+      // if so, recurse with (target, node.next)
+    return node.next ? list.contains(target, node.next) : false;
   };
 
   return list;
