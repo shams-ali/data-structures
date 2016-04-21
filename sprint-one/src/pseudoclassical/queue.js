a992a092040bae41eb6ac68d2f7b804c272e2708
shams-ali <shams.ali0216@gmail.com>
2016-04-21 09:44:50 -0700

    Finished Pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,52 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+//create  base object with basic properties
+  
+  this.storage = {};
+  this.storageLength = Object.keys(this.storage).length;
+  
 };
+
+//add methods to method object
+Queue.prototype.enqueue = function(value) {
+  if (this.storageLength === undefined) {
+    this.storage[0] = value;
+  } else {
+    this.storage[this.storageLength] = value;
+  }
+  this.storageLength = Object.keys(this.storage).length;
+};
+
+Queue.prototype.dequeue = function() {
+  var res = this.storage[0];
+  delete this.storage[0];
+  //update storage object with new key 
+  //subtract current key - 1
+
+  //for in loop over object
+  //storage[+(key)-1] = object[key]
+  for (var key in this.storage) {
+    this.newKey = +key - 1;
+    this.storage[this.newKey] = this.storage[key];
+    delete this.storage[key];
+  }
+  this.storageLength = Object.keys(this.storage).length;
+  return res;
+};
+
+Queue.prototype.size = function() {
+  this.storageLength = Object.keys(this.storage).length; 
+  return this.storageLength;
+};
+
+
+
+
+
+
+
+
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,37 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  //base object contains storage object, and length variable
+  this.storage = {};
+  this.length = 0;
+   //might fix later
+
+};
+
+
+   
+
+//make method for push, pop, size
+
+Stack.prototype.push = function(value) {
+  this.storage[this.length] = value;
+  this.length++;
+};
+
+Stack.prototype.pop = function() {
+  if (this.length === 0) {
+    return undefined;
+  } else {
+    this.length--;
+    var popped = this.storage[this.length];
+    //delete this.storage[length];
+    return popped;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.length > 0 ? this.length : 0;
 };
 
 
