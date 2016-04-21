970974e2e7a0e156c6f4c84ffac22d41f04d0aa8
shams-ali <shams.ali0216@gmail.com>
2016-04-20 22:20:19 -0700

    finished stack prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,52 @@
+
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  //base object contains storage object, and length variable
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.length = 0;
+   //might fix later
+
+  return newStack;
 };
 
+//make object to store methods
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+   
 var stackMethods = {};
+
+//make method for push, pop, size
+
+stackMethods.push = function(value) {
+  this.storage[this.length] = value;
+  this.length++;
+};
+
+stackMethods.pop = function() {
+  if (this.length === 0) {
+    return undefined;
+  } else {
+    this.length--;
+    var popped = this.storage[this.length];
+    //delete this.storage[length];
+    return popped;
+  }
+};
+
+stackMethods.size = function() {
+  return this.length > 0 ? this.length : 0;
+};
+
+
+
+
+
 
 
