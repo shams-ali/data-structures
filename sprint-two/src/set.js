3a828bb220fef5deceef746ad540752c154eef4f
Shams Ali <ali.s90@outlook.com>
2016-04-22 00:41:49 -0700

    finished set using underscore functions

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,29 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.push(item);
+
 };
 
 setPrototype.contains = function(item) {
+  return _.some (this._storage, function(value) {
+    return value === item;
+  });
 };
 
 setPrototype.remove = function(item) {
+  this._storage = _.reject (this._storage, function(value) {
+    return value === item;
+  });
 };
 
+var set = Set();
 /*
  * Complexity: What is the time complexity of the above functions?
  */
