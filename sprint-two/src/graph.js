5957249fc7e1c5d1bbaae472af153b9ba7c998ae
Shams Ali <ali.s90@outlook.com>
2016-04-22 19:22:03 -0700

    finished graph and hashtable

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,46 +2,64 @@
 
 // ------------------------
 // Instantiate a new graph
+
 var Graph = function() {
+  this.vertices = {};
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.vertices[node] = [];
+  //console.log(this.storage);
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return _.contains(Object.keys(this.vertices), node +'');
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this.vertices[node];
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return _.contains(this.vertices[fromNode], toNode);
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
+
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.vertices[fromNode].push(toNode);
+  this.vertices[toNode].push(fromNode);
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  this.vertices[fromNode] = _.reject (this.vertices[fromNode], function(value) {
+    return toNode === value;
+  });
+  this.vertices[toNode] = _.reject (this.vertices[toNode], function(value) {
+    return fromNode === value;
+  });
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  _.each (this.vertices, function(value, key, array) {
+    cb(key);
+  });
 };
 
+var graph = new Graph();
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,24 +3,53 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.hash = [];
+  this.hash.length = this._limit;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var newArray = [];
+  newArray.push([k, v]);
+  if (!Array.isArray(this.hash[index])) {
+    this.hash[index] = newArray;
+  } else if ( _.contains (this.hash[index][0], k)) {
+    this.hash[index] = newArray;
+  } else {
+    this.hash[index].push([k, v]);
+  }
+
+
+  //console.log(this.hash);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this.hash[index].length; i++) {
+    if (this.hash[index][i][0] === k) {
+      return this.hash[index][i][1];
+    }
+  }
+  // _.reduce (this.hash[index], function(prev, curr) {
+  //   // console.log(value[0])
+  //   // console.log(value[1])
+  //   // console.log(k)
+  //   if (curr[0] === k) {
+  //     return value[1];
+  //   }
+  //}, false);
+  //return this.hash[index][0][1];
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this.hash[index][0] = [];
 };
 
+var hashTable = new HashTable();
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
